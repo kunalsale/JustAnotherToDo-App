a9b3c7b8a165079b061069a868b75dbd398b92b0
kunalsale <kunal.sale94@gmail.com>
2021-08-27 14:07:00 +0530

    Changes: 1. AddToDoFragment added 2. UI test case written for fragment navigation from list to add fragment

--- app/src/androidTest/java/com/ksale/justanothertodoapp/ToDoListFragmentTest.kt
+++ app/src/androidTest/java/com/ksale/justanothertodoapp/ToDoListFragmentTest.kt
@@ -1,11 +1,17 @@
 package com.ksale.justanothertodoapp
 
+import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import junit.framework.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,11 +19,27 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class ToDoListFragmentTest {
 
+    lateinit var toDoFragmentScenario: FragmentScenario<ToDoListFragment>
+    lateinit var navHostController: TestNavHostController
+
     @Before
     fun setUp() {
-        launchFragmentInContainer<ToDoListFragment>(
-            themeResId = R.style.Base_Theme_AppCompat
+        // Create Test nav host controller
+        navHostController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
         )
+
+        toDoFragmentScenario = launchFragmentInContainer(themeResId = R.style.Base_Theme_AppCompat) {
+            ToDoListFragment().also { fragment ->
+                fragment.viewLifecycleOwnerLiveData.observeForever { viewLifecycleOwner ->
+                    if (viewLifecycleOwner != null) {
+                        // The fragment’s view has just been created
+                        navHostController.setGraph(R.navigation.todo_navigation)
+                        Navigation.setViewNavController(fragment.requireView(), navHostController)
+                    }
+                }
+            }
+        }
     }
 
     @Test
@@ -32,5 +54,18 @@ class ToDoListFragmentTest {
             .check(matches(isDisplayed()))
         onView(withId(R.id.ivNoData))
             .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun checkFABClick() {
+        onView(withId(R.id.fabAddToDo))
+            .perform(click())
+    }
+
+    @Test
+    fun checkIfFABClickNavigatesToAddFragment() {
+        onView(withId(R.id.fabAddToDo))
+            .perform(click())
+        assertTrue("Should be same destination",navHostController.currentDestination?.id == R.id.addToDoFragment)
     }
 }
--- app/src/main/java/com/ksale/justanothertodoapp/ToDoListFragment.kt
+++ app/src/main/java/com/ksale/justanothertodoapp/ToDoListFragment.kt
@@ -5,18 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ToDoListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ToDoListFragment : Fragment() {
+
+    lateinit var fabAddToDo: FloatingActionButton
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_to_do_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fabAddToDo = view.findViewById(R.id.fabAddToDo)
+        fabAddToDo.setOnClickListener {
+            findNavController().navigate(R.id.action_toDoListFragment_to_addToDoFragment)
+        }
     }
 }
